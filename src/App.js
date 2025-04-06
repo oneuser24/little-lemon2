34a250bc9378b701335bf7cd43524190876b958b
oneuser24 <bohdan.senyuk@colorado.edu>
2025-04-05 19:49:36 -0600

    Simplify the initial boilerplate app

--- src/App.js
+++ src/App.js
@@ -3,9 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        Project start
-      </header>
+        Homepage
     </div>
   );
 }
